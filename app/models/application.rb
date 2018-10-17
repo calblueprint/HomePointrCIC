3e77e126aec1a2f3bdfed926e55e690d0836e04d
Florence <florence.lau@berkeley.edu>
2018-10-17 15:18:01 -0700

    Finished priority and hidden methods

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,4 +2,19 @@ class Application < ApplicationRecord
 	enum status: { rejected: 0, received: 1, interview: 2, housed: 3 }
 	belongs_to :property
 	belongs_to :info
+
+	def hidden	
+	"""
+	Returns whether this application should be hidden or not.
+	"""
+		if status == 0
+			return true
+		elsif status > 1
+			return false
+		else
+			if info.tenant.priority < 2
+				return true
+			return false
+		end
+	end
 end
