1b51435f9da5b6699ccbf04de3575081eba305b0
Kevin Deguzman <deguzmankevin@hotmail.com>
2018-11-13 21:01:56 -0800

    fix sign in redirects

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,34 @@
 class ApplicationController < ActionController::Base
 	include Pundit
-	# after_action :verify_authorized
+	before_action :require_login, except: [:new, :create]
 	before_action :configure_permitted_parameters, if: :devise_controller?
-
+	# after_action :verify_authorized
+	
 	rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+	
 	private
 
-  def user_not_authorized(exception)
-  	policy_name = exception.policy.class.to_s.underscore
+  	def user_not_authorized(exception)
+  		policy_name = exception.policy.class.to_s.underscore
 
-   	flash[:error] = "You cannot #{policy_name}.#{exception.query}"
-    redirect_to errors_show_path
-  end
+   		flash[:error] = "You cannot #{policy_name}.#{exception.query}"
+    	redirect_to errors_show_path
+  	end
+
+  	def require_login
+    	unless user_signed_in?
+      	flash[:error] = "You must be logged in to access this section"
+      	redirect_to unauthenticated_root_path
+    	end
+  	end
+
+  	def after_sign_in_path_for(resource)
+  		if current_user.type == 'Landlord'
+  			landlord_path(current_user)
+  		else 
+  			referral_agency_path(current_user)
+  		end
+  	end
 
 	protected
 
@@ -19,6 +36,5 @@ class ApplicationController < ActionController::Base
 	  devise_parameter_sanitizer.permit(:sign_up) do |u|
 	    u.permit(:name, :type, :email, :password, :phone, :address)
 	  end
-	end
-
+	end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,10 @@ Rails.application.routes.draw do
   resources :applications, :only => [:new, :create, :edit, :update, :show, :destroy]
   resources :infos, :only => [:create]
   devise_for :users
+  devise_scope :user do
+  unauthenticated do
+    root 'devise/sessions#new', as: :unauthenticated_root
+  end
+ end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
