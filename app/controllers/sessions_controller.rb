5db412e324be4666a7c473ce6be5389aebe44405
Kevin Deguzman <deguzmankevin@hotmail.com>
2018-11-14 00:29:04 -0800

    try sessionscontroller stuff

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,35 @@
+#frozen_string_literal: true
+
+class SessionsController < Devise::SessionsController
+  before_action :require_login, except: [:new, :create]
+  before_action :configure_sign_in_params, only: [:create]
+
+ # GET /resource/sign_in
+  def new
+    super
+  end
+
+  #POST /resource/sign_in
+  def create
+    super
+  end
+
+  #DELETE /resource/sign_out
+  def destroy
+    super
+  end
+
+  protected
+
+  #If you have extra params to permit, append them to the sanitizer.
+  def configure_sign_in_params
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  end
+
+  def require_login
+      unless user_signed_in?
+        flash[:error] = "You must be logged in to access this section"
+        redirect_to unauthenticated_root_path
+      end
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,38 @@
 Rails.application.routes.draw do
+  get 'home/index'
   get 'errors/show'
   resources :referral_agencies, :only => [:create, :show, :update, :edit, :destroy]
   resources :landlords, :only => [:create, :show, :update, :edit, :destroy]
   resources :properties, :only => [:new, :create, :update, :index, :edit, :show, :destroy]
   resources :tenants, :only => [:new, :create, :edit, :update, :show, :index, :destroy]
-  resources :applications, :only => [:new, :create, :edit, :update, :show, :destroy]
+  resources :applications, :only => [:index, :new, :create, :edit, :update, :show, :destroy]
   # resources :properties, :only => [:create, :update, :show, :destroy]
   resources :properties
   # resources :tenants, :only => [:create, :update, :show, :destroy]
   resources :tenants
   resources :applications, :only => [:create, :show, :destroy]
   resources :infos, :only => [:create]
-  devise_for :users
+  devise_for :users, controllers: {
+        sessions: 'sessions',
+        registrations: "registrations"
+      }
+  devise_scope :user do
+  authenticated do 
+    root 'home#index', as: :authenticated_root
+  end
+
+  unauthenticated do
+    root 'sessions#new', as: :unauthenticated_root
+  end
 
   namespace :api do
-  	resources :landlords, :referral_agencies, :infos, :applications
-    resources :properties, :tenants do
-      member do
-        delete 'delete_image_attachment/:image_id', :action => 'delete_image_attachment'
+    resources :landlords, :referral_agencies, :infos, :applications
+      resources :properties, :tenants do
+        member do
+          delete 'delete_image_attachment/:image_id', :action => 'delete_image_attachment'
+        end
       end
     end
-  end
-
-  devise_scope :user do
-  unauthenticated do
-    root 'devise/sessions#new', as: :unauthenticated_root
-  end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
