248a6a84b78fe06a364e2dd74002ab522c171297
Kevin Deguzman <deguzmankevin@hotmail.com>
2018-11-14 21:20:43 -0800

    tabs to spaces

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,32 +1,32 @@
 class ApplicationController < ActionController::Base
-	include Pundit
-	before_action :require_login
-	before_action :configure_permitted_parameters, if: :devise_controller?
-	# after_action :verify_authorized
-	
-	rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
-	
-	private
+    include Pundit
+    before_action :require_login
+    before_action :configure_permitted_parameters, if: :devise_controller?
+    # after_action :verify_authorized
+    
+    rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+    
+    private
 
-  	def user_not_authorized(exception)
-  		policy_name = exception.policy.class.to_s.underscore
+    def user_not_authorized(exception)
+        policy_name = exception.policy.class.to_s.underscore
 
-   		flash[:error] = "You cannot #{policy_name}.#{exception.query}"
-    	redirect_to errors_show_path
-  	end
+        flash[:error] = "You cannot #{policy_name}.#{exception.query}"
+        redirect_to errors_show_path
+    end
 
-  	def require_login
-    	unless user_signed_in?
-      	flash[:error] = "You must be logged in to access this section"
-      	redirect_to unauthenticated_root_path
-    	end
-  	end
+    def require_login
+        unless user_signed_in?
+        flash[:error] = "You must be logged in to access this section"
+        redirect_to unauthenticated_root_path
+        end
+    end
 
-	protected
+    protected
 
-	def configure_permitted_parameters
-	  devise_parameter_sanitizer.permit(:sign_up) do |u|
-	    u.permit(:name, :type, :email, :password, :phone, :address)
-	  end
-	end 
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up) do |u|
+        u.permit(:name, :type, :email, :password, :phone, :address)
+      end
+    end 
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,12 @@
 class HomeController < ApplicationController
-	# before_action :require_login, except: :index
+  # before_action :require_login, except: :index
   def index
-  	if user_signed_in?
-  		if current_user.type == 'Landlord'
-  			redirect_to landlord_path(current_user)
-  		else 
-  			redirect_to referral_agency_path(current_user)
-  		end
-  	end
+    if user_signed_in?
+      if current_user.type == 'Landlord'
+        redirect_to landlord_path(current_user)
+      else 
+        redirect_to referral_agency_path(current_user)
+      end
+    end
   end
 end
