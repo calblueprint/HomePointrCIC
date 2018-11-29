7fe23f44bacf9cba073bcc85fe90009a9df7b357
Kevin Deguzman <deguzmankevin@hotmail.com>
2018-11-28 21:06:39 -0800

    convert ListView to stateful component

--- app/javascript/components/ListView.jsx
+++ app/javascript/components/ListView.jsx
@@ -3,41 +3,53 @@ import PropTypes from "prop-types";
 import { Card, Col, Row, Button } from "antd";
 import "antd/dist/antd.css";
 
-const ListView = props => {
-  const handleClick = property_id => {
+class ListView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      resources: this.props.resources,
+      type: this.props.type
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(property_id) {
     window.location.assign("/properties/" + property_id);
-  };
-  return props.resources.map(resource => {
-    return (
-      <div
-        style={{
-          background: "#ECECEC",
-          padding: "30px",
-          marginLeft: "20%",
-          marginRight: "20%",
-          marginTop: "2%"
-        }}
-      >
-        <Row gutter={16}>
-          {props.type == "property" ? (
-            <Card title={resource.location} bordered={false}>
-              {resource.description}
-              <Button type="default" href={"/properties/" + resource.id}>
-                View Info
-              </Button>
-            </Card>
-          ) : (
-            <Card title={resource.name} bordered={false}>
-              <Button type="default" href={"/tenants/" + resource.id}>
-                View Info
-              </Button>
-            </Card>
-          )}
-        </Row>
-      </div>
-    );
-  });
-};
+  }
+
+  render() {
+    return this.state.resources.map(resource => {
+      return (
+        <div
+          style={{
+            background: "#ECECEC",
+            padding: "30px",
+            marginLeft: "20%",
+            marginRight: "20%",
+            marginTop: "2%"
+          }}
+        >
+          <Row gutter={16}>
+            {this.state.type == "property" ? (
+              <Card title={resource.location} bordered={false}>
+                {resource.description}
+                <Button type="default" href={"/properties/" + resource.id}>
+                  View Info
+                </Button>
+              </Card>
+            ) : (
+              <Card title={resource.name} bordered={false}>
+                <Button type="default" href={"/tenants/" + resource.id}>
+                  View Info
+                </Button>
+              </Card>
+            )}
+          </Row>
+        </div>
+      );
+    });
+  }
+}
 
 ListView.propTypes = {
   resources: PropTypes.array
