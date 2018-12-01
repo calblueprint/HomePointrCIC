a40d008501d5f503fbc06219e657a5d824bec002
Kevin Deguzman <deguzmankevin@hotmail.com>
2018-12-01 02:37:53 -0800

    style fixes

--- app/javascript/components/RAHomeView.jsx
+++ app/javascript/components/RAHomeView.jsx
@@ -1,22 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
-import 'antd/dist/antd.css';
+import "antd/dist/antd.css";
 import ListView from "./ListView.jsx";
 import SplitViewContainer from "./SplitViewContainer.jsx";
 
 class RAHomeView extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+	constructor(props) {
+		super(props);
+	}
 
-  render() {
-  	const leftComponent = <ListView resources={this.props.tenants} type='tenant' />;
-  	const rightComponent = <ListView resources={this.props.properties} type='property' />;
+	render() {
+		const leftComponent = (
+			<ListView resources={this.props.tenants} type="tenant" />
+		);
+		const rightComponent = (
+			<ListView resources={this.props.properties} type="property" />
+		);
 
-  	return (
-  		<SplitViewContainer leftComponent={leftComponent} rightComponent={rightComponent} />
-  		)
-  }
+		return (
+			<SplitViewContainer
+				leftComponent={leftComponent}
+				rightComponent={rightComponent}
+			/>
+		);
+	}
 }
 
 export default RAHomeView;
