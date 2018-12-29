2be3a1c0a6056e2271ff09833a845841b51853a5
arpanpal <arpan.pal1234@gmail.com>
2018-12-29 00:04:11 -0600

    remove serializers

--- app/serializers/application_serializer.rb
+++ app/serializers/application_serializer.rb
@@ -1,21 +1,21 @@
-class ApplicationSerializer < ActiveModel::Serializer
-  attributes :status,
-             :application_tenant,
-             :application_property,
-             :application_referral_agency
-  def application_tenant
-    info = object.info
-    tenant =  info.tentant
-  end
-
-  def application_property
-    property = object.property
-  end
-
-  def application_referral_agency 
-    info = object.info
-    tenant =  info.tentant 
-    referral_agency = tenant.referral_agency
-  end
-
-end
+# class ApplicationSerializer < ActiveModel::Serializer
+#   attributes :status,
+#              :application_tenant,
+#              :application_property,
+#              :application_referral_agency
+#   def application_tenant
+#     info = object.info
+#     tenant =  info.tentant
+#   end
+#
+#   def application_property
+#     property = object.property
+#   end
+#
+#   def application_referral_agency
+#     info = object.info
+#     tenant =  info.tentant
+#     referral_agency = tenant.referral_agency
+#   end
+#
+# end
--- app/serializers/property_serializer.rb
+++ app/serializers/property_serializer.rb
@@ -1,19 +1,19 @@
-class PropertySerializer < ActiveModel::Serializer
-  attributes :status,
-             :property_tenants,
-             :property_applications,
-  def property_tenants
-    tenants = object.tenants
-  end
+# class PropertySerializer < ActiveModel::Serializer
+#   attributes :status,
+#              :property_tenants,
+#              :property_applications,
+#   def property_tenants
+#     tenants = object.tenants
+#   end
 
   # def application_property
   #   property = object.property
   # end
 
-  # def application_referral_agency 
+  # def application_referral_agency
   #   info = object.info
-  #   tenant =  info.tentant 
+  #   tenant =  info.tentant
   #   referral_agency = tenant.referral_agency
   # end
-
-end
+#
+# end
