cec18c6a6b8d4761ff630e5c873bbff2052ddd91
Wilson Wang <wilsonw926@gmail.com>
2018-12-29 00:19:40 -0800

    non confirm user email

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,8 @@ class User < ApplicationRecord
   has_many :emails, dependent: :destroy
   validates :name, :emails, :type, :address, :phone, presence: true
   validates :type, inclusion: { in: ["Landlord", "ReferralAgency"] }
-  devise :multi_email_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, 
-         :multi_email_confirmable, :multi_email_validatable
+  devise :registerable, :recoverable, :rememberable, :validatable, 
+        :multi_email_validatable, :multi_email_authenticatable 
 
 	def will_save_change_to_email?
 	end
