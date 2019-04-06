b2dc3be53db277e0a26b593f1825567554c86eae
Cathleen Jia <cathleenjia@berkeley.edu>
2019-04-06 15:58:13 -0700

    navbar styling

--- app/javascript/components/individual/NavBar.jsx
+++ app/javascript/components/individual/NavBar.jsx
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import { Avatar, Button } from "antd";
 import APIRoutes from "helpers/api_routes";
 import "antd/dist/antd.css";
+import logo from 'images/logo.png';
 
 class NavBar extends React.Component {
   constructor(props) {
@@ -40,41 +41,65 @@ class NavBar extends React.Component {
   renderAvatar() {
     return (
       <React.Fragment key="avatar">
-        <Avatar size={64} icon="user" />
+        <Avatar size={36} icon="user" onClick={this.renderDialog}/>
       </React.Fragment>
     );
   }
 
+  toggleDialog() {
+    console.log("hello");
+    if (document.querySelector("#navbar-dialog").style.display != "block") {
+      document.querySelector("#navbar-dialog").style.display = "block";
+    } else {
+      document.querySelector("#navbar-dialog").style.display = "none";
+    }
+  }
+
+  returnHome() {
+    window.location = "/";
+  }
+
   render() {
     return (
-      <div
-        style={{
-          background: "#FFFFFF",
-          height: "64px",
-          borderBottom: "1px solid #C4C4C4"
-        }}
-      >
-        {[
-          this.renderEmail(),
-          this.renderAvatar(),
-          <Button
-            key="edit"
-            type="default"
-            onClick={this.handleEdit}
-            style={{ float: "right" }}
-          >
-            Edit Account
-          </Button>,
-          <Button
-            key="logout"
-            type="default"
-            onClick={this.handleLogout}
-            style={{ float: "right" }}
-          >
-            Logout
-          </Button>
-        ]}
+      // <div
+      //   style={{
+      //     background: "#FFFFFF",
+      //     height: "64px",
+      //     borderBottom: "1px solid #C4C4C4"
+      //   }}
+      // >
+      //   {[
+      //     this.renderEmail(),
+      //     this.renderAvatar(),
+      //     <Button
+      //       key="edit"
+      //       type="default"
+      //       onClick={this.handleEdit}
+      //       style={{ float: "right" }}
+      //     >
+      //       Edit Account
+      //     </Button>,
+      //     <Button
+      //       key="logout"
+      //       type="default"
+      //       onClick={this.handleLogout}
+      //       style={{ float: "right" }}
+      //     >
+      //       Logout
+      //     </Button>
+      //   ]}
+      // </div>
+      <div>
+        <div className="navbar">
+          <img className="home-button-logo" onClick={this.returnHome} src={logo}/>
+          <div className="navbar-avatar" onClick={this.toggleDialog}>{this.renderAvatar()}</div>
+        </div>
+        <div id="navbar-dialog">
+          <Button key="edit" type="default" onClick={this.handleEdit} className="navbar-dialog-button">Edit account</Button>
+          <Button key="logout" type="default" onClick={this.handleLogout} className="navbar-dialog-button">Logout</Button>
+        </div>
       </div>
+
     );
   }
 }
