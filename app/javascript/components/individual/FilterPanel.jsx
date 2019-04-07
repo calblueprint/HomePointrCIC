7baaf402869323d586b23a1894c7b2e6374409dc
Florence <florence.lau@berkeley.edu>
2019-04-06 19:11:16 -0700

    Restyled but broke functionality :(

--- app/javascript/components/individual/FilterPanel.jsx
+++ app/javascript/components/individual/FilterPanel.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
 import 'antd/dist/antd.css';
-import { Menu, Dropdown, Icon, Radio, InputNumber, Button, Checkbox, DatePicker } from 'antd';
+import { Menu, Dropdown, Icon, Radio, InputNumber, Button, Checkbox, DatePicker, Modal, Row, Col, Slider } from 'antd';
 import moment from 'moment';
 import APIRoutes from 'helpers/api_routes';
+import '../../../assets/stylesheets/FilterPanel.css';
 
 class FilterPanel extends React.Component {
   constructor(props) {
@@ -12,255 +13,275 @@ class FilterPanel extends React.Component {
       location: [],
       capacity: 0,
       rent_min: 0,
-      rent_max: 9999,
+      rent_max: 5000,
       number_of_bedrooms: 0,
       property_type: [],
       housing_type: [],
       date_available: moment().format("YYYY-MM-DD"),
-      isOpen: false,
+      isDateOpen: false,
+      isGuestsOpen: false,
+      isLocationOpen: false,
+      isPropertyTypeOpen: false,
+      isHousingTypeOpen: false,
+      isRentOpen: false,
+      actual: {
+        location: [],
+        capacity: 0,
+        rent_min: 0,
+        rent_max: 5000,
+        number_of_bedrooms: 0,
+        property_type: [],
+        housing_type: [],
+        date_available: moment().format("YYYY-MM-DD"),
+      },
     };
     this.handleClick = this.handleClick.bind(this);
     this.handleCheckbox = this.handleCheckbox.bind(this);
     this.handleDatePicker = this.handleDatePicker.bind(this);
     this.passState = this.passState.bind(this);
     this.toggleOpen = this.toggleOpen.bind(this);
+    this.sliderChanges = this.sliderChanges.bind(this);
+    this.closeAll = this.closeAll.bind(this);
+    this.getInitialValues = this.getInitialValues.bind(this);
+    this.componentDidMount = this.componentDidMount.bind(this);
+  }
+
+  componentDidMount = () => {
+    this.getInitialValues();
   }
 
   handleClick = (event, filter_name) => {
-    this.setState({[filter_name]: event.target.value})
+    this.setState({[filter_name]: event.target.value});
   }
 
   handleCheckbox = (checkedValues, filter_name) => {
-    this.setState({[filter_name]: checkedValues})
+    this.setState({[filter_name]: checkedValues});
   }
 
   handleDatePicker = (date, dateString) => {
-    this.setState({date_available: date})
+    this.setState({date_available: date});
   }
 
   handleInputNumber = (value, filter_name) => {
-    this.setState({[filter_name]: value})
+    this.setState({[filter_name]: value});
   }
 
   passState = () => {
-    this.toggleOpen()
-    this.props.applyFilter(this.state)
+    this.props.applyFilter(this.state);
+    this.closeAll();
   }
 
-  toggleOpen = () => {
-    this.setState({isOpen: !this.state.isOpen})
-    this.handleState()
+  closeAll = () => {
+    this.setState({
+      isDateOpen: false,
+      isGuestsOpen: false,
+      isLocationOpen: false,
+      isPropertyTypeOpen: false,
+      isHousingTypeOpen: false,
+      isRentOpen: false,
+    });
   }
 
-  handleState() {
-    if (this.props.selectedTenant) {
+  toggleOpen = (filter_name) => {
+    this.closeAll();
+    this.setState({[filter_name]: true});
+  }
+
+  sliderChanges([value1, value2]) {
+    this.setState({ 
+      rent_min: value1,
+      rent_max: value2,
+    });
+  }
+
+  getInitialValues = () => {
+    if (this.props.tenants[0]) {
+      let tenant = this.props.tenants[0];
       this.setState({
-        location: [this.props.selectedTenant.location],
-        rent_min: this.props.selectedTenant.rent_min,
-        rent_max: this.props.selectedTenant.rent_max,
-        number_of_bedrooms: this.props.selectedTenant.number_of_bedrooms,
-        property_type: [this.props.selectedTenant.property_type],
-        housing_type: [this.props.selectedTenant.housing_type],
-        date_available: moment(this.props.selectedTenant.date_needed),
-      })
-    }
+        location: [tenant.location],
+        capacity: tenant.capacity,
+        rent_min: tenant.rent_min,
+        rent_max: tenant.rent_max,
+        number_of_bedrooms: tenant.number_of_bedrooms,
+        property_type: [tenant.property_type],
+        housing_type: [tenant.housing_type],
+        date_available: moment(tenant.date_needed),
+      });
+    };
   }
 
   render() {
     const CheckboxGroup = Checkbox.Group;
-    let menu = (
-      <div
-        style={{
-          background: "white",
-          marginLeft: "10%",
-          marginRight: "10%",
-          padding: "10px",
-          borderStyle: "solid",
-        }}
-      >
-        <div
-          style={{
-            borderBottom: "1px solid",
-            borderColor: "#E5E5E5",
-            paddingBottom: "15px",
-          }}
+    const marks = {
+      0: "$0",
+      2500: "$2500",
+      5000: "$5000"
+    };
+
+    const buttons = (
+      <div className="buttons-panel">
+        <Button
+          type="default"
+          style={{ border: 'none' }}
+          onClick={this.toggleOpen}
         >
-          <div
-            style={{
-              paddingBottom: "10px",
-              fontWeight: "bold",
-            }}
-          >
-            Location
-          </div>
-          <CheckboxGroup
-            options={this.props.location_options}
-            defaultValue={this.state.location}
-            onChange={(e) => this.handleCheckbox(e, "location")}
-          />
-        </div>
-        <div
-          style={{
-            borderBottom: "1px solid",
-            borderColor: "#E5E5E5",
-            paddingBottom: "15px",
-          }}
-        >
-          <div
-            style={{
-              paddingBottom: "10px",
-              paddingTop: "10px",
-              fontWeight: "bold",
-            }}
-          >
-          Property Type
-          </div>
-          <CheckboxGroup
-            options={this.props.property_options}
-            defaultValue={this.state.property_type}
-            onChange={(e) => this.handleCheckbox(e, "property_type")}
-          />
-        </div>
-        <div
-          style={{
-            borderBottom: "1px solid",
-            borderColor: "#E5E5E5",
-            paddingBottom: "15px",
-          }}
-        >
-          <div
-            style={{
-              paddingBottom: "10px",
-              paddingTop: "10px",
-              fontWeight: "bold",
-            }}
-          >
-          Housing Type
-          </div>
-          <CheckboxGroup
-            options={this.props.housing_options}
-            defaultValue={this.state.housing_type}
-            onChange={(e) => this.handleCheckbox(e, "housing_type")}
-          />
-        </div>
-        <div
-          style={{
-            borderBottom: "1px solid",
-            borderColor: "#E5E5E5",
-            paddingBottom: "15px",
-          }}
-        >
-          <div
-            style={{
-              paddingBottom: "10px",
-              paddingTop: "10px",
-              fontWeight: "bold",
-            }}
-          >
-        Capacity
-        </div>
-        <InputNumber min={0} defaultValue={0} onChange={(e) => this.handleCheckbox(e, "capacity")} />
-        </div>
-        <div
-          style={{
-            borderBottom: "1px solid",
-            borderColor: "#E5E5E5",
-            paddingBottom: "15px",
-          }}
-        >
-          <div
-            style={{
-              paddingBottom: "10px",
-              paddingTop: "10px",
-              fontWeight: "bold",
-            }}
-          >
-        Number of Bedrooms
-        </div>
-        <InputNumber min={0} defaultValue={this.state.size} onChange={(e) => this.handleCheckbox(e, "number_of_bedrooms")} />
-        </div>
-        <div
-          style={{
-            borderBottom: "1px solid",
-            borderColor: "#E5E5E5",
-            paddingBottom: "15px",
-          }}
-        >
-          <div
-            style={{
-              paddingBottom: "10px",
-              paddingTop: "10px",
-              fontWeight: "bold",
-            }}
-          >
-        Rent
-        </div>
-        Min<InputNumber min={0} defaultValue={this.state.rent_min} onChange={(e) => this.handleCheckbox(e, "rent_min")} />
-        Max<InputNumber min={0} defaultValue={this.state.rent_max} onChange={(e) => this.handleCheckbox(e, "rent_max")} />
-        </div>
-        <div
-          style={{
-            paddingBottom: "15px",
-          }}
-        >
-          <div
-            style={{
-              paddingBottom: "10px",
-              paddingTop: "10px",
-              fontWeight: "bold",
-            }}
-          >
-        Date Available
-        </div>
-        <DatePicker
-          onChange={(date, dateString) => this.handleDatePicker(date, dateString)}
-          defaultValue={moment(this.state.date_available)}
-        />
-        </div>
-      <Button
-        type="primary"
-        onClick={this.passState}
-      >
-        <div
-          style={{
-            paddingBottom: "15px",
-          }}
+          Clear
+        </Button>
+        <Button
+          type="default"
+          style={{ border: 'none' }}
+          onClick={this.passState}
         >
           Apply
+        </Button>
+      </div>
+    );
+
+    const dateFilter = (
+      <div className="options">
+        <DatePicker
+          onChange={(date, dateString) => this.handleDatePicker(date, dateString)}
+          value={moment(this.state.date_available)}
+        />
+        {buttons}
+      </div>
+    );
+
+    const guestsFilter = (
+      <div className="options">
+        Bedrooms
+        <div className="guests">
+          <InputNumber
+            min={0}
+            value={this.state.number_of_bedrooms}
+            onChange={(e) => this.handleCheckbox(e, "number_of_bedrooms")}
+          />
         </div>
-      </Button>
-      <Button
-        type="danger"
-        onClick={this.toggleOpen}
-      >
-        <div
-          style={{
-            paddingBottom: "15px",
-          }}
-        >
-          Cancel
+        Capacity
+        <div className="guests">
+          <InputNumber
+            min={0}
+            value={this.state.capacity}
+            onChange={(e) => this.handleCheckbox(e, "capacity")}
+          />
         </div>
-      </Button>
-    </div>
+        {buttons}
+      </div>
+    );
+
+    const locationFilter = (
+      <div className="options">
+        <CheckboxGroup
+          options={this.props.location_options}
+          value={this.state.location}
+          onChange={(e) => this.handleCheckbox(e, "location")}
+        />
+        {buttons}
+      </div>
+    );
+
+    const propertyTypeFilter = (
+      <div className="options">
+        <CheckboxGroup
+          options={this.props.property_options}
+          value={this.state.property_type}
+          onChange={(e) => this.handleCheckbox(e, "property_type")}
+        />
+        {buttons}
+      </div>
+    );
+
+    const housingTypeFilter = (
+      <div className="options">
+        <CheckboxGroup
+          options={this.props.housing_options}
+          value={this.state.housing_type}
+          onChange={(e) => this.handleCheckbox(e, "housing_type")}
+        />
+        {buttons}
+      </div>
+    );
+
+    const rentFilter = (
+      <div className="options">
+        <Row gutter={10}>
+          <Col span={6}>
+            <InputNumber
+              min={0}
+              max={5000}
+              style={{ width: 80 }}
+              value={this.state.rent_min}
+              onChange={(e) => this.handleCheckbox(e, "rent_min")}
+            />
+          </Col>
+          <Col span={12}>
+            <Slider
+              range marks={marks}
+              min={0}
+              max={5000}
+              style={{ width: 200, paddingLeft: 10 }}
+              defaultValue={typeof this.state.rent_min === 'number' && typeof this.state.rent_max === 'number' ? [this.state.rent_min, this.state.rent_max] : [0, 5000]}
+              onChange={this.sliderChanges}/>
+          </Col>
+          <Col span={6}>
+            <InputNumber
+              min={0}
+              max={5000}
+              style={{ width: 80 }}
+              value={this.state.rent_max}
+              onChange={(e) => this.handleCheckbox(e, "rent_max")}
+            />
+          </Col>
+        </Row>
+        {buttons}
+      </div>
     );
 
     return (
-    <div>
-      <Dropdown overlay={menu} visible={this.state.isOpen} trigger={['click']}>
-        <a className="ant-dropdown-link" href="#">
-          <div
-            style={{
-            background: "white",
-            marginLeft: "10%",
-            marginRight: "10%",
-            }}
-            onClick={this.toggleOpen}
-          >
-            Filters <Icon type="down" />
-          </div>
-        </a>
-      </Dropdown>
-    </div>
+      <div className="navbar">
+        <Dropdown overlay={dateFilter} visible={this.state.isDateOpen} trigger={['click']}>
+          <a className="ant-dropdown-link" href="#">
+            <div onClick={(e) => this.toggleOpen("isDateOpen")}>
+              Date needed
+            </div>
+          </a>
+        </Dropdown>
+        <Dropdown overlay={guestsFilter} visible={this.state.isGuestsOpen} trigger={['click']}>
+          <a className="ant-dropdown-link" href="#">
+            <div onClick={(e) => this.toggleOpen("isGuestsOpen")}>
+              Guests
+            </div>
+          </a>
+        </Dropdown>
+        <Dropdown overlay={locationFilter} visible={this.state.isLocationOpen} trigger={['click']}>
+          <a className="ant-dropdown-link" href="#">
+            <div onClick={(e) => this.toggleOpen("isLocationOpen")}>
+              Location
+            </div>
+          </a>
+        </Dropdown>
+        <Dropdown overlay={propertyTypeFilter} visible={this.state.isPropertyTypeOpen} trigger={['click']}>
+          <a className="ant-dropdown-link" href="#">
+            <div onClick={(e) => this.toggleOpen("isPropertyTypeOpen")}>
+              Property type
+            </div>
+          </a>
+        </Dropdown>
+        <Dropdown overlay={housingTypeFilter} visible={this.state.isHousingTypeOpen} trigger={['click']}>
+          <a className="ant-dropdown-link" href="#">
+            <div onClick={(e) => this.toggleOpen("isHousingTypeOpen")}>
+              Housing type
+            </div>
+          </a>
+        </Dropdown>
+        <Dropdown overlay={rentFilter} visible={this.state.isRentOpen} trigger={['click']}>
+          <a className="ant-dropdown-link" href="#">
+            <div onClick={(e) => this.toggleOpen("isRentOpen")}>
+              Rent
+            </div>
+          </a>
+        </Dropdown>
+      </div>
     );
   }
 }
@@ -269,7 +290,7 @@ FilterPanel.propTypes = {
   location_options: PropTypes.array,
   property_options: PropTypes.array,
   housing_options: PropTypes.array,
-  selectedTenant: PropTypes.object
+  tenants: PropTypes.object
 };
 
 export default FilterPanel;
