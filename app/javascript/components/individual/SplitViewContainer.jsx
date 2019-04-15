d2e1fd7d0d8d11eb185b06a2a1f0a4ffc519e654
Wilson Wang <wilsonw926@gmail.com>
2019-04-14 22:17:38 -0700

    spaces

--- app/javascript/components/individual/SplitViewContainer.jsx
+++ app/javascript/components/individual/SplitViewContainer.jsx
@@ -2,39 +2,39 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const SplitViewContainer = props => (
-	<div
-		key="splitViewContainer" style={{
-			display: "flex",
-			justifyContent: "space-around",
-			flexDirection: "row",
-			height: "90%"
-		}}
-	>
-		<div key="left"
-			style={{
-				width: "480px",
-				height: "100vh",
-				display: "inline-block",
-				overflow: "scroll",
-				borderLeft: "1px solid gray",
-			}}
-		>
-			{props.leftComponent}
-		</div>
+  <div
+    key="splitViewContainer" style={{
+      display: "flex",
+      justifyContent: "space-around",
+      flexDirection: "row",
+      height: "90%"
+    }}
+  >
+    <div key="left"
+      style={{
+        width: "480px",
+        height: "100vh",
+        display: "inline-block",
+        overflow: "scroll",
+        borderLeft: "1px solid gray",
+      }}
+    >
+      {props.leftComponent}
+    </div>
 
-		<div key="right"
-			style={{
-				width: "960px",
-				height: "100vh",
-				display: "inline-block",
-				overflow: "scroll",
-				paddingLeft: "10px",
-				borderLeft: "1px solid gray"
-			}}
-		>
-			{props.rightComponent}
-		</div>
-	</div>
+    <div key="right"
+      style={{
+        width: "960px",
+        height: "100vh",
+        display: "inline-block",
+        overflow: "scroll",
+        paddingLeft: "10px",
+        borderLeft: "1px solid gray"
+      }}
+    >
+      {props.rightComponent}
+    </div>
+  </div>
 );
 
 SplitViewContainer.propTypes = {
