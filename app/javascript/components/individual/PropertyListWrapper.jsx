5c543a9f2c06073e789696e8ada9b984e5c3d23d
Wilson Wang <wilsonw926@gmail.com>
2019-04-15 01:45:24 -0700

    spacing

--- app/javascript/components/individual/PropertyListWrapper.jsx
+++ app/javascript/components/individual/PropertyListWrapper.jsx
@@ -5,65 +5,65 @@ import ListView from "./../ListView.jsx";
 import moment from 'moment';
 
 class PropertyListWrapper extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			properties: props.properties,
-		}
-		this.handleFilter = this.handleFilter.bind(this);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      properties: props.properties,
+    }
+    this.handleFilter = this.handleFilter.bind(this);
+  }
 
-	handleFilter(filters) {
-		let updatedProperties;
-		updatedProperties = this.props.properties.filter(p => {
-			return (
-				filters["capacity"] <= p.capacity && 
-				filters["rent_min"] <= p.rent && 
-				filters["rent_max"] >= p.rent && 
-				filters["number_of_bedrooms"] <= p.number_of_bedrooms
-			);
-		})
-		if (filters["date_available"]) {
-			const dateToMoment = moment(filters["date_available"]);
-			updatedProperties = updatedProperties.filter(p => {
-				return (
-					(dateToMoment.isAfter(p.date_available) ||
-					dateToMoment.isSame(p.date_available))
-				);
-			})
-		}
-		if (filters["location"].length > 0) {
-			updatedProperties = updatedProperties.filter(p => {
-				return (
-					filters["location"].indexOf(p.location) > -1
-				);
-			})
-		}
-		if (filters["property_type"].length > 0) {
-			updatedProperties = updatedProperties.filter(p => {
-				return (
-					filters["property_type"].indexOf(p.property_type) > -1
-				);
-			})
-		}
-		if (filters["housing_type"].length > 0) {
-			updatedProperties = updatedProperties.filter(p => {
-				return (
-					filters["housing_type"].indexOf(p.housing_type) > -1
-				);
-			})
-		}
-		this.setState({properties: updatedProperties})
-	}
-	
-	render() {
-		return (
-			<div key="PropertyListWrapper">
-				<FilterPanel {...this.props} applyFilter={this.handleFilter} />
-				<ListView avatar={true} resources={this.state.properties} property_modal={true} type="property" checkbox={true} CheckboxChange={this.props.CheckboxChange}/>
-			</div>
-		);
-	}
+  handleFilter(filters) {
+    let updatedProperties;
+    updatedProperties = this.props.properties.filter(p => {
+      return (
+        filters["capacity"] <= p.capacity && 
+        filters["rent_min"] <= p.rent && 
+        filters["rent_max"] >= p.rent && 
+        filters["number_of_bedrooms"] <= p.number_of_bedrooms
+      );
+    })
+    if (filters["date_available"]) {
+      const dateToMoment = moment(filters["date_available"]);
+      updatedProperties = updatedProperties.filter(p => {
+        return (
+          (dateToMoment.isAfter(p.date_available) ||
+          dateToMoment.isSame(p.date_available))
+        );
+      })
+    }
+    if (filters["location"].length > 0) {
+      updatedProperties = updatedProperties.filter(p => {
+        return (
+          filters["location"].indexOf(p.location) > -1
+        );
+      })
+    }
+    if (filters["property_type"].length > 0) {
+      updatedProperties = updatedProperties.filter(p => {
+        return (
+          filters["property_type"].indexOf(p.property_type) > -1
+        );
+      })
+    }
+    if (filters["housing_type"].length > 0) {
+      updatedProperties = updatedProperties.filter(p => {
+        return (
+          filters["housing_type"].indexOf(p.housing_type) > -1
+        );
+      })
+    }
+    this.setState({properties: updatedProperties})
+  }
+  
+  render() {
+    return (
+      <div key="PropertyListWrapper">
+        <FilterPanel {...this.props} applyFilter={this.handleFilter} />
+        <ListView avatar={true} resources={this.state.properties} property_modal={true} type="property" checkbox={true} CheckboxChange={this.props.CheckboxChange}/>
+      </div>
+    );
+  }
 }
 
 PropertyListWrapper.propTypes = {
