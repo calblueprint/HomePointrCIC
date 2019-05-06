5dac26b4fb1c1619ff098b34666867d47ed26279
Wilson Wang <wilsonw926@gmail.com>
2019-05-06 16:25:12 -0700

    images

---
+++ app/javascript/components/individual/Carousel.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+
+class Carousel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      images: this.props.images,
+      imgCount: this.props.images.length,
+      currentImg: 0
+    }
+  }
+
+  onRightClick = () => {
+    if (this.state.currentImg < this.state.imgCount - 1) {
+      this.setState((state) => {
+        return {currentImg: state.currentImg + 1}
+      });
+    } else {
+      this.setState((state) => {
+        return {currentImg: 0}
+      });
+    }
+  }
+
+  onLeftClick = () => {
+    if (this.state.currentImg > 0) {
+      this.setState((state) => {
+        return {currentImg: state.currentImg - 1}
+      });
+    } else {
+      this.setState((state) => {
+        return {currentImg: state.imgCount - 1}
+      });
+    }
+  }
+
+
+  render() {
+    if (this.props.location === "modal") {
+      if (this.state.imgCount !== 1) {
+        return (
+          <div className="carousel-modal">
+            <img className="carousel-img-modal" src={this.state.images[this.state.currentImg]["url"]}/>
+            <div className="left-arrow-modal">
+              <img src="/assets/left-arrow.png" onClick={this.onLeftClick} className="arrow-img"/>
+            </div>
+            <div className="right-arrow-modal">
+              <img src="/assets/right-arrow.png" onClick={this.onRightClick} className="arrow-img"/>
+            </div>
+            <div className="expand-text h3-expand">
+              Click and hold image to expand.
+            </div>
+          </div>
+        );
+      } else {
+        return(
+          <div className="carousel-modal">
+            <img className="carousel-img-modal" src={this.state.images[this.state.currentImg]["url"]}/>
+          </div>
+        );
+      }
+    } else if (this.props.location === "view") {
+      if (this.state.imgCount !== 1) {
+        return (
+          <div className="carousel-view">
+            <img className="carousel-img-view" src={this.state.images[this.state.currentImg]["url"]}/>
+            <div className="left-arrow-view">
+              <img src="/assets/left-arrow.png" onClick={this.onLeftClick} className="arrow-img"/>
+            </div>
+            <div className="right-arrow-view">
+              <img src="/assets/right-arrow.png" onClick={this.onRightClick} className="arrow-img"/>
+            </div>
+          </div>
+        );
+      } else {
+        return(
+          <div className="carousel-view">
+            <img className="carousel-img-view" src={this.state.images[this.state.currentImg]["url"]}/>
+          </div>
+        );
+      }
+    }
+  }
+}
+
+export default Carousel;
