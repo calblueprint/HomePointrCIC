6b77c71bf9ed33ca9689d479a448023676eae5c1
mir1995 <mir00021@hotmail.com>
2019-06-19 11:11:29 +0100

    Fixing spaces and casting global variables to local

--- lib/recommendation.rb
+++ lib/recommendation.rb
@@ -1,11 +1,10 @@
-# suggestion for optimising: only pass in property id rather than full object?
 module Recommendation
 	
 	# tenant_features: utilities and furniture are missing from tenants schema
 	TENANT_PROPERTY_FEATURES = ["mobility_aids",
-								"accessible_shower",
-								"car_parking", 
-								"lift_access"]	
+															"accessible_shower",
+															"car_parking", 
+															"lift_access"]	
 
 	def recommend_properties(properties)
 		
@@ -14,22 +13,22 @@ module Recommendation
 	
 		recommended = Hash.new(0)
 
-		@user_features =  self.attributes.slice(*TENANT_PROPERTY_FEATURES)
+		user_features =  self.attributes.slice(*TENANT_PROPERTY_FEATURES)
 											.values
 											.map{|x| if x then x= 1 else x= 0 end }
-		@user_preferred_location = Geolocator.get_coord(self.location)	 
+		user_preferred_location = Geolocator.get_coord(self.location)	 
 		properties.each do |property|
 
-			@property_features =  property.attributes.slice(*TENANT_PROPERTY_FEATURES)
+			property_features =  property.attributes.slice(*TENANT_PROPERTY_FEATURES)
 											.values
 											.map{|x| if x then x=1 else x= 0 end }
 	
-			score = dot_product( u=@property_features, v=@user_features ) +
+			score = dot_product( u=property_features, v=user_features ) +
 					Math.exp( -
-					@user_preferred_location.distance_to(Geolocator.get_coord(property.location))
+					user_preferred_location.distance_to(Geolocator.get_coord(property.location))
 					)
 			
-			recommended[property] += score
+			recommended[property] = score
 		end
 
 		sorted_properties_score = recommended.sort_by {|key,value| value}.reverse!
@@ -47,33 +46,24 @@ module Recommendation
 		@@default_units = :miles # others :kms, :nms, :meters
 		@@default_formula = :sphere
 		@@request_timeout = 20
-		@@key = 'fziv9Az2xPjh1ZEjHkc3T2TTNUuodeC0'
+		@@key = 
 		
 		# maybe there is no need to write this method?
 		def self.get_coord(address)
-
 			return geocode(address.capitalize, :bias => 'uk')
-
 		end
-
 	end
 
 	def dot_product(u,v)
-
 		sum = 0
 		u.each_index do |i|
 			sum += u[i] * v[i]
 		end
-
 		return sum
 	end
 
-
-	def generate_candidates(properties)
-		
+	def generate_candidates(properties)		
 		return properties.select { |e| e[:location] ==  self.location }
-
 	end	
-
 
 end
